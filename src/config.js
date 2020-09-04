ac781ec1b1302c834dfed90391e1e199cb7e31f8
Aaron <amsegal93@gmail.com>
2020-09-04 14:28:03 -0400

    Fixed migration script on test server and datebase

--- src/config.js
+++ src/config.js
@@ -2,4 +2,8 @@ module.exports = {
     PORT: process.env.PORT,
     NODE_ENV: process.env.NODE_ENV,
     DATABASE_URL: process.env.DATABASE_URL
+    /*PORT: 8000,
+    NODE_ENV: 'development',
+    DATABASE_URL: "postgresql://dunder_mifflin@localhost/dnd-monster-creator"*/
+    
 }
